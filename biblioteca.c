4e00817169a8c10436eb1ef1a7611b5f6ca4afce
marisa-brt <68127210+marisa-brt@users.noreply.github.com>
2020-08-25 16:10:14 -0300

    Update biblioteca.c

--- biblioteca.c
+++ biblioteca.c
@@ -1,9 +1,9 @@
 #include "biblioteca.h"
 #include <math.h>
 
-//Biblioteca com funÁıes necess·rias para determinaÁ„o da trajetÛria de voo de um bal„o meteorolÛgico com hÈlio
+//Biblioteca com fun√ß√µes necess√°rias para determina√ß√£o da trajet√≥ria de voo de um bal√£o meteorol√≥gico com h√©lio
 
-double grav(double r)//C·lculo da aceleraÁ„o da gravidade real
+double grav(double r)//C√°lculo da acelera√ß√£o da gravidade real
 {
     double C, g;
     //M_t = 5.972e24; //Massa da Terra
@@ -13,50 +13,50 @@ double grav(double r)//C·lculo da aceleraÁ„o da gravidade real
 return g;
 }
 
-// DeterminaÁ„o de atmosfera padr„o internacional
+// Determina√ß√£o de atmosfera padr√£o internacional
 
-double Temp(double r){//C·lculo da temperatura em funÁ„o da altitude em K
+double Temp(double r){//C√°lculo da temperatura em fun√ß√£o da altitude em K
     double T0, grad_t, T, alt;
-    alt = r-R_T; //Altitude em relaÁ„o ao nÌvel do mar
-    if (alt <= 11000){ //C·lculo da temperatura na Troposfera
+    alt = r-R_T; //Altitude em rela√ß√£o ao n√≠vel do mar
+    if (alt <= 11000){ //C√°lculo da temperatura na Troposfera
         T0 = 299.3734;
-        grad_t = -4.9687e-3; //gradiente tÈrmico especÌfico da camada atmosfÈrica em K/m
+        grad_t = -4.9687e-3; //gradiente t√©rmico espec√≠fico da camada atmosf√©rica em K/m
         T = T0 + grad_t*alt;
     }
-    if ((11000 < alt) && (alt <= 25000)){ //C·lculo da temperatura na baixa estratosfera
+    if ((11000 < alt) && (alt <= 25000)){ //C√°lculo da temperatura na baixa estratosfera
         T0 = 216.5;
         grad_t = 0; //temperatura constante (??)
         T = T0 + grad_t*(alt-11000);
     }
-    if (alt > 25000){//C·lculo da temperatura na alta estratosfera
+    if (alt > 25000){//C√°lculo da temperatura na alta estratosfera
         T0 = 216.5;
-        grad_t = 0.003; //gradiente tÈrmico especÌfico da camada atmosfÈrica em K/m
+        grad_t = 0.003; //gradiente t√©rmico espec√≠fico da camada atmosf√©rica em K/m
         T = T0 + grad_t*(alt-25000);
     }
  return T;
 }
 
-double Pres(double r, double T, double g){ //C·lculo da press„o atmosfÈrica local em Pa
+double Pres(double r, double T, double g){ //C√°lculo da press√£o atmosf√©rica local em Pa
     double T0, grad_t, P0, P, C;
-    double alt = r - R_T; //Altitude em relaÁ„o ao nÌvel do mar
-    if (alt <= 11000){ //C·lculo da press„o na troposfera
+    double alt = r - R_T; //Altitude em rela√ß√£o ao n√≠vel do mar
+    if (alt <= 11000){ //C√°lculo da press√£o na troposfera
         T0 = 299.3734;
         P0 = 104036.51034;
-        grad_t = -4.9687e-3; //gradiente tÈrmico especÌfico da camada atmosfÈrica em K/m
+        grad_t = -4.9687e-3; //gradiente t√©rmico espec√≠fico da camada atmosf√©rica em K/m
         C = - g/(Ra*grad_t);
         P = P0 * pow((T/T0),C);
     }
-    if ((11000 < alt) && (alt <= 25000)){ //C·lculo da press„o na baixa estratosfera
+    if ((11000 < alt) && (alt <= 25000)){ //C√°lculo da press√£o na baixa estratosfera
         T0 = 216.5;
         P0 = 22552;
-        grad_t = 0; //gradiente tÈrmico especÌfico da camada atmosfÈrica em K/m
+        grad_t = 0; //gradiente t√©rmico espec√≠fico da camada atmosf√©rica em K/m
         C = - g*(alt-11000)/(Ra*T0);
         P = P0 * exp(C);
     }
-    if (alt > 25000){ //C·lculo da press„o na alta estratosfera
+    if (alt > 25000){ //C√°lculo da press√£o na alta estratosfera
         T0 = 216.5;
         P0 = 2481;
-        grad_t = 0.003; //gradiente tÈrmico especÌfico da camada atmosfÈrica em K/m
+        grad_t = 0.003; //gradiente t√©rmico espec√≠fico da camada atmosf√©rica em K/m
         C = - g/(Ra*grad_t);
         P = P0 * pow((T/T0),C);
     }
@@ -65,96 +65,96 @@ double Pres(double r, double T, double g){ //C·lculo da press„o atmosfÈrica loca
 
 double dens(double r, double T, double P, double g){ // Densidade do ar por altitude (kg/m^3)
     double T0, grad_t, ro_0, C, P0, ro;
-    double alt = r - R_T; //Altitude em relaÁ„o ao nÌvel do mar
-    if (alt <= 11000){ //C·lculo da densidade na troposfera
+    double alt = r - R_T; //Altitude em rela√ß√£o ao n√≠vel do mar
+    if (alt <= 11000){ //C√°lculo da densidade na troposfera
         T0 = 299.3734;
         ro_0 = 1.225;
-        grad_t = -4.9687e-3; //gradiente tÈrmico especÌfico da camada atmosfÈrica em K/m (G3)
+        grad_t = -4.9687e-3; //gradiente t√©rmico espec√≠fico da camada atmosf√©rica em K/m (G3)
         C = -((g/(Ra*grad_t))+1);
         ro = ro_0 * pow((T/T0),C);
     }
-    if ((11000 < alt) && (alt <= 25000)){ //C·lculo da densidade na baixa estratosfera
+    if ((11000 < alt) && (alt <= 25000)){ //C√°lculo da densidade na baixa estratosfera
         T0 = 216.5;
         P0 = 22552;
         ro_0 = P0/(Ra*T0);
-        grad_t = 0; //gradiente tÈrmico especÌfico da camada atmosfÈrica em K/m
+        grad_t = 0; //gradiente t√©rmico espec√≠fico da camada atmosf√©rica em K/m
         C = - g*(alt-11000)/(Ra*T0);
         ro = ro_0 * exp(C);
     }
-    if (alt > 25000){//C·lculo da densidade na alta estratosfera
+    if (alt > 25000){//C√°lculo da densidade na alta estratosfera
         T0 = 216.5;
         P0 = 2481;
         ro_0 = P0/(Ra*T0);
-        grad_t = 0.003; //gradiente tÈrmico especÌfico da camada atmosfÈrica em K/m
+        grad_t = 0.003; //gradiente t√©rmico espec√≠fico da camada atmosf√©rica em K/m
         C = -((g/(Ra*grad_t))+1);
         ro = ro_0 * pow((T/T0),C);
     }
  return ro;
 }
 
-double dens_helio(double P, double T){ //Densidade local do hÈlio
+double dens_helio(double P, double T){ //Densidade local do h√©lio
     double ro_h = P/(R_h*T);
     return ro_h;
 }
 
-double volume(double m_h, double ro_h){ //Volume local do bal„o
+double volume(double m_h, double ro_h){ //Volume local do bal√£o
     double vol = m_h/ro_h;
     return vol;
 }
 
-//DeterminaÁ„o do raio inicial do bal„o em funÁ„o da altitude
+//Determina√ß√£o do raio inicial do bal√£o em fun√ß√£o da altitude
 double raio_bi(double ro_h, double m_h)
 {
-    double r_bi = pow((3*m_h/(4*M_PI*ro_h)),1.0/3.0); //raio do bal„o conforme densidade de hÈlio
+    double r_bi = pow((3*m_h/(4*M_PI*ro_h)),1.0/3.0); //raio do bal√£o conforme densidade de h√©lio
     return r_bi;
 }
 
-//DeterminaÁ„o do raio do bal„o pelo relaÁ„o tens„o x deformaÁ„o (1)
+//Determina√ß√£o do raio do bal√£o pelo rela√ß√£o tens√£o x deforma√ß√£o (1)
 /*double raio_b(double P, double T, double m_h, double ro_h, double r_b, double vol, double e_0, double r_b0){
-    //double r_bi = pow((3*m_h/(4*M_PI*ro_h)),1.0/3.0); //raio do bal„o conforme densidade inicial
+    //double r_bi = pow((3*m_h/(4*M_PI*ro_h)),1.0/3.0); //raio do bal√£o conforme densidade inicial
     //double vol = 4.0*M_PI*pow(r_b,3.0)/3.0;
-    double P_h = R_h*ro_h*T; //Press„o interna no bal„o
-    //double drp = -3.205936e-6 - 4.886422e-5*(pow(0.999954, P_h));//derivada do raio do bal„o pela press„o interna
+    double P_h = R_h*ro_h*T; //Press√£o interna no bal√£o
+    //double drp = -3.205936e-6 - 4.886422e-5*(pow(0.999954, P_h));//derivada do raio do bal√£o pela press√£o interna
     double e = e_0*r_b0*r_b0/(r_b*r_b);
-    double beta = 1.71583389E7 -4.81401394E8*pow(0.064193536,r_b); //r_b*r_b/(2.0*e*drp) + 3.0*r_b*r_b*r_b*(P_h-P)/(2.0*e_0*r_b0*r_b0); //MÛdulo de young calculado (troposfera)
-    double delta_v = (m_h/ro_h) - vol; //VariaÁ„o do volume
+    double beta = 1.71583389E7 -4.81401394E8*pow(0.064193536,r_b); //r_b*r_b/(2.0*e*drp) + 3.0*r_b*r_b*r_b*(P_h-P)/(2.0*e_0*r_b0*r_b0); //M√≥dulo de young calculado (troposfera)
+    double delta_v = (m_h/ro_h) - vol; //Varia√ß√£o do volume
     double r_bf = r_b*beta/(beta - (P_h-P)*r_b/2*e);// + (pow(3.0*delta_v/(4.0*M_PI),(1.0/3.0)));
     return r_bf;
 }*/
 
-//C·lculo de dr_b/dt para a determinaÁ„o do raio do bal„o no Runge-Kutta
+//C√°lculo de dr_b/dt para a determina√ß√£o do raio do bal√£o no Runge-Kutta
 double drb_dt(double d_r, double T, double P, double r_b, double ro_h, double g, double m, double P_h, double drb_dpm)
 {
     double grad_t = -4.9687e-3;
-    double drb_dtf = (drb_dpm*(d_r/T)*((-P_h*grad_t) + (P*g/R_h)))/(1.0+(3.0*P_h*drb_dpm/r_b));//D˙vida em relaÁ„o ao sinal do grad_t
+    double drb_dtf = (drb_dpm*(d_r/T)*((P_h*grad_t) + (P*g/R_h)))/(1.0+(3.0*P_h*drb_dpm/r_b));
     return drb_dtf;
 }
 
-//C·lculo do arrasto gerado pelo bal„o na direÁ„o radial
+//C√°lculo do arrasto gerado pelo bal√£o na dire√ß√£o radial
 double arrasto_r(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
 {
-    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //MÛdulo do vetor velocidade total
+    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //M√≥dulo do vetor velocidade total
     double arrasto = -ro*(M_PI)*v_modulo*(v_r-vvento_r)*pow(r_b,2)*Ca/2;
 return arrasto;
 }
 
-//C·lculo do arrasto gerado pelo bal„o na direÁ„o theta
+//C√°lculo do arrasto gerado pelo bal√£o na dire√ß√£o theta
 double arrasto_theta(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
 {
-    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //MÛdulo do vetor velocidade total
+    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //M√≥dulo do vetor velocidade total
     double arrasto = -ro*(M_PI)*v_modulo*(v_theta-vvento_theta)*pow(r_b,2)*Ca/2;
 return arrasto;
 }
 
-//C·lculo do arrasto gerado pelo bal„o na direÁ„o lambda
+//C√°lculo do arrasto gerado pelo bal√£o na dire√ß√£o lambda
 double arrasto_lambda(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
 {
-    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //MÛdulo do vetor velocidade total
+    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //M√≥dulo do vetor velocidade total
     double arrasto = -ro*(M_PI)*v_modulo*(v_lambda-vvento_lambda)*pow(r_b,2)*Ca/2;
 return arrasto;
 }
 
-//AceleraÁ„o na direÁ„o radial
+//Acelera√ß√£o na dire√ß√£o radial
 double d2r(double r, double theta, double d_r, double d_theta, double d_lambda, double m, double vol, double g, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
 {
     double s = sin(theta);
@@ -163,7 +163,7 @@ double d2r(double r, double theta, double d_r, double d_theta, double d_lambda, 
     return d2r_r;
 }
 
-//AceleraÁ„o na direÁ„o theta
+//Acelera√ß√£o na dire√ß√£o theta
 double d2theta(double r, double theta, double d_r, double d_theta, double d_lambda, double m, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
 {
     double s = sin(theta);
@@ -173,8 +173,8 @@ double d2theta(double r, double theta, double d_r, double d_theta, double d_lamb
     return d2theta_r;
 }
 
-//AceleraÁ„o na direÁ„o lambda
-double d2lambda(double r, double theta, double d_r,double d_theta, double d_lambda,double m, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)//AceleraÁ„o na direÁ„o lambda
+//Acelera√ß√£o na dire√ß√£o lambda
+double d2lambda(double r, double theta, double d_r,double d_theta, double d_lambda,double m, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)//Acelera√ß√£o na dire√ß√£o lambda
 {
     double s = sin(theta);
     double c = cos(theta);
