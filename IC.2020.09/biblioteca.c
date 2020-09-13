26666e35007ab024634b0b4f92c65d413dcbbfbd
marisa-brt <68127210+marisa-brt@users.noreply.github.com>
2020-09-13 11:39:34 -0300

    Add files via upload

---
+++ IC.2020.09/biblioteca.c
@@ -0,0 +1,187 @@
+
+#include "biblioteca.h"
+#include <math.h>
+
+//Biblioteca com funções necessárias para determinação da trajetória de voo de um balão meteorológico com hélio
+
+double grav(double r)//Cálculo da aceleração da gravidade real
+{
+    double C, g;
+    //M_t = 5.972e24; //Massa da Terra
+    //C_G = 6.67408e-11; //Constante gravitacional
+    C = 3.9857605e14; // C = C_G*M_t
+    g = C/(r*r);
+return g;
+}
+
+// Determinação de atmosfera padrão internacional
+
+double Temp(double r){//Cálculo da temperatura em função da altitude em K
+    double T0, grad_t, T, alt;
+    alt = r-R_T; //Altitude em relação ao nível do mar
+    if (alt <= 11000){ //Cálculo da temperatura na Troposfera
+        T0 = 299.3734;
+        grad_t = -4.9687e-3; //gradiente térmico específico da camada atmosférica em K/m
+        T = T0 + grad_t*alt;
+    }
+    if ((11000 < alt) && (alt <= 25000)){ //Cálculo da temperatura na baixa estratosfera
+        T0 = 216.5;
+        grad_t = 0; //temperatura constante (??)
+        T = T0 + grad_t*(alt-11000);
+    }
+    if (alt > 25000){//Cálculo da temperatura na alta estratosfera
+        T0 = 216.5;
+        grad_t = 0.003; //gradiente térmico específico da camada atmosférica em K/m
+        T = T0 + grad_t*(alt-25000);
+    }
+ return T;
+}
+
+double Pres(double r, double T, double g){ //Cálculo da pressão atmosférica local em Pa
+    double T0, grad_t, P0, P, C;
+    double alt = r - R_T; //Altitude em relação ao nível do mar
+    if (alt <= 11000){ //Cálculo da pressão na troposfera
+        T0 = 299.3734;
+        P0 = 104036.51034;
+        grad_t = -4.9687e-3; //gradiente térmico específico da camada atmosférica em K/m
+        C = - g/(Ra*grad_t);
+        P = P0 * pow((T/T0),C);
+    }
+    if ((11000 < alt) && (alt <= 25000)){ //Cálculo da pressão na baixa estratosfera
+        T0 = 216.5;
+        P0 = 22552;
+        grad_t = 0; //gradiente térmico específico da camada atmosférica em K/m
+        C = - g*(alt-11000)/(Ra*T0);
+        P = P0 * exp(C);
+    }
+    if (alt > 25000){ //Cálculo da pressão na alta estratosfera
+        T0 = 216.5;
+        P0 = 2481;
+        grad_t = 0.003; //gradiente térmico específico da camada atmosférica em K/m
+        C = - g/(Ra*grad_t);
+        P = P0 * pow((T/T0),C);
+    }
+ return P;
+}
+
+double dens(double r, double T, double P, double g){ // Densidade do ar por altitude (kg/m^3)
+    double T0, grad_t, ro_0, C, P0, ro;
+    double alt = r - R_T; //Altitude em relação ao nível do mar
+    if (alt <= 11000){ //Cálculo da densidade na troposfera
+        T0 = 299.3734;
+        ro_0 = 1.225;
+        grad_t = -4.9687e-3; //gradiente térmico específico da camada atmosférica em K/m (G3)
+        C = -((g/(Ra*grad_t))+1);
+        ro = ro_0 * pow((T/T0),C);
+    }
+    if ((11000 < alt) && (alt <= 25000)){ //Cálculo da densidade na baixa estratosfera
+        T0 = 216.5;
+        P0 = 22552;
+        ro_0 = P0/(Ra*T0);
+        grad_t = 0; //gradiente térmico específico da camada atmosférica em K/m
+        C = - g*(alt-11000)/(Ra*T0);
+        ro = ro_0 * exp(C);
+    }
+    if (alt > 25000){//Cálculo da densidade na alta estratosfera
+        T0 = 216.5;
+        P0 = 2481;
+        ro_0 = P0/(Ra*T0);
+        grad_t = 0.003; //gradiente térmico específico da camada atmosférica em K/m
+        C = -((g/(Ra*grad_t))+1);
+        ro = ro_0 * pow((T/T0),C);
+    }
+ return ro;
+}
+
+double dens_helio(double P, double T){ //Densidade local do hélio
+    double ro_h = P/(R_h*T);
+    return ro_h;
+}
+
+double volume(double m_h, double ro_h){ //Volume local do balão
+    double vol = m_h/ro_h;
+    return vol;
+}
+
+//Determinação do raio inicial do balão em função da altitude
+double raio_bi(double ro_h, double m_h)
+{
+    double r_bi = pow((3*m_h/(4*M_PI*ro_h)),1.0/3.0); //raio do balão conforme densidade de hélio
+    return r_bi;
+}
+
+//Determinação do raio do balão pelo relação tensão x deformação (1)
+/*double raio_b(double P, double T, double m_h, double ro_h, double r_b, double vol, double e_0, double r_b0){
+    //double r_bi = pow((3*m_h/(4*M_PI*ro_h)),1.0/3.0); //raio do balão conforme densidade inicial
+    //double vol = 4.0*M_PI*pow(r_b,3.0)/3.0;
+    double P_h = R_h*ro_h*T; //Pressão interna no balão
+    //double drp = -3.205936e-6 - 4.886422e-5*(pow(0.999954, P_h));//derivada do raio do balão pela pressão interna
+    double e = e_0*r_b0*r_b0/(r_b*r_b);
+    double beta = 1.71583389E7 -4.81401394E8*pow(0.064193536,r_b); //r_b*r_b/(2.0*e*drp) + 3.0*r_b*r_b*r_b*(P_h-P)/(2.0*e_0*r_b0*r_b0); //Módulo de young calculado (troposfera)
+    double delta_v = (m_h/ro_h) - vol; //Variação do volume
+    double r_bf = r_b*beta/(beta - (P_h-P)*r_b/2*e);// + (pow(3.0*delta_v/(4.0*M_PI),(1.0/3.0)));
+    return r_bf;
+}*/
+
+//Cálculo de dr_b/dt para a determinação do raio do balão no Runge-Kutta
+double drb_dt(double d_r, double T, double P, double r_b, double ro_h, double g, double m, double P_h, double drb_dpm)
+{
+    double grad_t = -4.9687e-3;
+    double drb_dtf = (drb_dpm*(d_r/T)*((-P_h*grad_t) + (P*g/R_h)))/(1.0+(3.0*P_h*drb_dpm/r_b));//Dúvida em relação ao sinal do grad_t
+    return drb_dtf;
+}
+
+//Cálculo do arrasto gerado pelo balão na direção radial
+double arrasto_r(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
+{
+    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //Módulo do vetor velocidade total
+    double arrasto = -ro*(M_PI)*v_modulo*(v_r-vvento_r)*pow(r_b,2)*Ca/2;
+return arrasto;
+}
+
+//Cálculo do arrasto gerado pelo balão na direção theta
+double arrasto_theta(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
+{
+    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //Módulo do vetor velocidade total
+    double arrasto = -ro*(M_PI)*v_modulo*(v_theta-vvento_theta)*pow(r_b,2)*Ca/2;
+return arrasto;
+}
+
+//Cálculo do arrasto gerado pelo balão na direção lambda
+double arrasto_lambda(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
+{
+    double v_modulo = sqrt(pow(v_r-vvento_r,2) + pow(v_theta-vvento_theta,2) + pow(v_lambda-vvento_lambda,2)); //Módulo do vetor velocidade total
+    double arrasto = -ro*(M_PI)*v_modulo*(v_lambda-vvento_lambda)*pow(r_b,2)*Ca/2;
+return arrasto;
+}
+
+//Aceleração na direção radial
+double d2r(double r, double theta, double d_r, double d_theta, double d_lambda, double m, double vol, double g, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
+{
+    double s = sin(theta);
+    double arrasto = arrasto_r(d_r, d_theta*r, d_lambda*r*s, vol, ro, r_b, vvento_r, vvento_theta, vvento_lambda);
+    double d2r_r = (ro*vol*(g - OMEGA*OMEGA*r*s*s))/m - g + arrasto/m + OMEGA*r*s*s*(2*d_lambda + OMEGA) + r*d_theta*d_theta + r*s*s*d_lambda*d_lambda;
+    return d2r_r;
+}
+
+//Aceleração na direção theta
+double d2theta(double r, double theta, double d_r, double d_theta, double d_lambda, double m, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)
+{
+    double s = sin(theta);
+    double arrasto = arrasto_theta(d_r, d_theta*r, d_lambda*r*s, vol, ro, r_b, vvento_r, vvento_theta, vvento_lambda);
+    double sc = s*cos(theta);
+    double d2theta_r = (-ro*vol*OMEGA*OMEGA*sc)/m + arrasto/(r*m) + OMEGA*sc*(2*d_lambda + OMEGA) - 2*d_r*d_theta/r + sc*d_lambda*d_lambda;
+    return d2theta_r;
+}
+
+//Aceleração na direção lambda
+double d2lambda(double r, double theta, double d_r,double d_theta, double d_lambda,double m, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda)//Aceleração na direção lambda
+{
+    double s = sin(theta);
+    double c = cos(theta);
+    double arrasto = arrasto_lambda(d_r, d_theta*r, d_lambda*r*s, vol, ro, r_b,vvento_r, vvento_theta, vvento_lambda);
+    //double dvlambda_r = arrasto/(r*s*m) - 2*OMEGA*(v_r/r + v_theta*c/s) - 2*v_r*v_lambda/r - 2*v_theta*v_lambda*c/s;
+    double d2lambda_r = arrasto/(r*s*m) - 2*d_r/r*(OMEGA + d_lambda) - 2*d_theta*c/s*(OMEGA + d_lambda);
+    return d2lambda_r;
+}
+
---
+++ IC.2020.09/main.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "biblioteca.h" //Biblioteca com funções necessárias
+
+//Código para cálculo da trajetória e velocidade em coordenadas esféricas do voo de um balão meteorológico com hélio
+int main(int argc, char* argv[])
+{
+    double r, theta, lambda, vol, m, alt, h, r_b, r_b0, e, e_0, r_bf, m_h, g, T, P, P_h, ro, ro_h0, ro_h;
+    double d_r, d_lambda, d_theta, vvento_r, vvento_theta, vvento_lambda, drb_dpm, beta_e;
+    double t = 0.0; //Tempo
+    int cont_p = 0.0;//Contador para impressão do resultado de 1000 a 1000 passos
+    h = 0.01;//Passo
+    int result;
+
+    //if(argc != 6){
+        //printf("Usar: IC.2020.08.exe altitude latitude(graus) longitude(graus) massa_balao(balão + payload [kg]) empuxo(kgf)\n\n");
+        //exit(1);
+    //}
+
+    //Aplicação das variáveis dadas pelo usuário no código
+    alt = 850.0;//atof(argv[1]);
+    double latitude = -22.00548;//atof(argv[2]);
+    double longitude = -47.93405;//atof(argv[3]);
+    m = 6.2615;//atof(argv[4]);
+    //double empuxo = atof(argv[5]) + m;
+
+    r = alt+R_T;
+    theta = M_PI/2 - latitude*M_PI/180;
+    lambda = longitude*M_PI/180;
+
+    //Determina-se derivadas iniciais nulas
+    d_r = 0.0;
+    d_theta = 0.0;
+    d_lambda = 0.0;
+
+    //Determina-se as velocidade iniciais do vento (cte)
+    vvento_r = 0.0;
+    vvento_theta = 0.0;
+    vvento_lambda = 0.0;
+
+
+    FILE *arq; // chamando arquivo para escrita de dados
+    arq = fopen("Teste2.txt","w"); //Abrindo arquivo para escrita de dados
+    //printf("Defina as coordenadas (theta e lambda), o volume do balão,a massa e a altitude de lançamento \n");
+    //scanf("%lf %lf %lf %lf %lf", &theta, &lambda, &vol, &m, &alt);//Informações definidas pelo usuário
+
+    result = fprintf(arq,"#       Tempo      Altitude       Vel rad     Densid    Volume    Raio balão\n");
+
+    //theta = 1.54;//-0.3839724;
+    //lambda = 1.54;//2.3387412;
+    //vol_0 = 5/1.1226;
+    //m = 6.26;
+    //alt = 852.0;
+    //vol = 7.666;
+    //ro_h0 = 0.151;
+    //g = 9.8168;
+    //ro = 1.094;
+    g = grav(r); //aceleração da gravidade local
+    T = Temp(r); //temperatura local
+    P = Pres(r, T, g); //pressão local
+    ro = P/(Ra*T); //densidade atmosférica local
+    ro_h = P/(R_h*T); //densidade local do hélio
+    vol = 7.54644637536312716197;//empuxo/ro; //volume inicial do balão
+    m_h = ro_h * vol; //massa de hélio
+    e_0 = e = 0.0005;
+    r_b = r_b0 = raio_bi(ro_h, m_h);
+
+    /*
+    //Leitura de valores do raio do balão e do beta em arquivo
+    FILE *arq2 = fopen("rbteste.dat","rb");
+    double rbarq, betaarq, r_ba[2084], betaa[2084];
+    int cont_p2 = 1;
+
+        if(arq2 == NULL){
+            printf("Arquivo de raio de balão x beta não abre!");
+        }
+        else{
+            while((fscanf(arq2, "   %lf %lf", &rbarq, &betaarq)) != EOF){
+                r_ba[cont_p2] = rbarq;
+                betaa[cont_p2] = betaarq;
+                cont_p2 += 1;
+            }
+        }
+        int i = 1;
+        for(; i<cont_p2; ++i){
+            //printf(" %13.8lf %13.8lf\n", r_ba[i], betaa[i]);
+        }
+        fclose(arq2);
+        i = 1;*/
+
+    // Determinação das "inclinações" dos intervalos para o método de Hunge Kutta através de vetores de 6 elementos
+    double k1[7],k2[7],k3[7],k4[7];
+    //Cálculo dos vetores k's para cada elemento/variável posição e velocidade
+
+    result = fprintf(arq,"%13.8lf %13.8lf %13.8lf %13.8lf %13.8lf %13.8lf\n",  t ,  r - R_T,  d_r,  ro, vol,  r_b);//troquei d_theta e d_lambda por ro e vol
+
+    while ( r - R_T <= 32000){
+        g = grav(r);
+        T = Temp(r);
+        P = Pres(r, T, g);
+        ro = P/(Ra*T);
+        vol = 4.0*M_PI*pow(r_b,3.0)/3.0;//m_h/ro_h;
+        ro_h = m_h/vol;
+        P_h = ro_h*R_h*T;
+        e = e_0*r_b0*r_b0/(r_b*r_b);
+
+        /*
+        //Determinação de beta
+        while (fabs(r_b - r_ba[i])<=0.0001){
+            if (fabs(r_b - r_ba[i-1]<fabs(r_b - r_ba[i]))){
+                beta = betaa[i-1];
+                break;
+                }
+            else beta = betaa[i];
+            i++;
+        }*/
+        beta_e = 50990.23198*pow(r_b, -4.885887);
+        drb_dpm = r_b*r_b/(2.0*beta_e - 3.0*r_b*(P_h-P));
+
+        //calculo de k1 para cada uma das variáveis
+        k1[0] = h*drb_dt(d_r, T, P, r_b, ro_h, g, m, P_h, drb_dpm);
+        k1[1] = h*d_r;
+        k1[2] = h*d_theta;
+        k1[3] = h*d_lambda;
+        k1[4] = h*d2r(r,theta,d_r,d_theta, d_lambda, m, vol, g, ro, r_b, vvento_r, vvento_theta, vvento_lambda);
+        k1[5] = h*d2theta(r,theta,d_r,d_theta,d_lambda,m,vol, ro, r_b, vvento_r, vvento_theta, vvento_lambda);
+        k1[6] = h*d2lambda(r,theta,d_r,d_theta,d_lambda,m,vol, ro, r_b, vvento_r, vvento_theta, vvento_lambda);
+
+        //calculo k2
+        k2[0] = h*drb_dt(d_r+k1[4]/2.0, T, P, r_b+k1[0]/2.0, ro_h, g, m, P_h, drb_dpm);
+        k2[1] = h*(d_r+k1[4]/2.0);
+        k2[2] = h*(d_theta+k1[5]/2.0);
+        k2[3] = h*(d_lambda+k1[6]/2.0);
+        k2[4] = h*d2r(r+k1[1]/2.0,theta+k1[2]/2.0,d_r+k1[4]/2.0,d_theta+k1[5]/2.0, d_lambda+k1[6]/2.0,m,vol, g, ro, r_b+k1[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k2[5] = h*d2theta(r+k1[1]/2.0,theta+k1[2]/2.0,d_r+k1[4]/2.0,d_theta+k1[5]/2.0, d_lambda+k1[6]/2.0,m,vol, ro, r_b+k1[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k2[6] = h*d2lambda(r+k1[1]/2.0, theta+k1[2]/2.0, d_r+k1[4]/2.0,d_theta+k1[5]/2.0, d_lambda+k1[6]/2.0,m,vol, ro, r_b+k1[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+
+        //calculo de k3
+        k3[0] = h*drb_dt(d_r+k2[4]/2.0, T, P, r_b+k2[0]/2.0, ro_h, g, m, P_h, drb_dpm);
+        k3[1] = h*(d_r+k2[4]/2.0);
+        k3[2] = h*(d_theta+k2[5]/2.0);
+        k3[3] = h*(d_lambda+k2[6]/2.0);
+        k3[4] = h*d2r(r+k2[1]/2.0,theta+k2[2]/2.0,d_r+k2[4]/2.0,d_theta+k2[5]/2.0, d_lambda+k2[6]/2.0,m,vol, g, ro, r_b+k2[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k3[5] = h*d2theta(r+k2[1]/2.0,theta+k2[2]/2.0,d_r+k2[4]/2.0,d_theta+k2[5]/2.0, d_lambda+k2[6]/2.0,m,vol, ro, r_b+k2[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k3[6] = h*d2lambda(r+k2[1]/2.0,theta+k2[2]/2.0,d_r+k2[4]/2.0,d_theta+k2[5]/2.0, d_lambda+k2[6]/2.0,m,vol, ro, r_b+k2[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+
+        //calculo de k4
+        k4[0] = h*drb_dt(d_r+k3[4], T, P, r_b+k3[0], ro_h, g, m, P_h, drb_dpm);
+        k4[1] = h*(d_r+k3[4]);
+        k4[2] = h*(d_theta+k3[5]);
+        k4[3] = h*(d_lambda+k3[6]);
+        k4[4] = h*d2r(r+k3[1],theta+k3[2],d_r+k3[4],d_theta+k3[5], d_lambda+k3[6],m,vol, g, ro, r_b+k3[0], vvento_r, vvento_theta, vvento_lambda);
+        k4[5] = h*d2theta(r+k3[1],theta+k3[2],d_r+k3[4],d_theta+k3[5], d_lambda+k3[6],m,vol, ro, r_b+k3[0], vvento_r, vvento_theta, vvento_lambda);
+        k4[6] = h*d2lambda(r+k3[1],theta+k3[2], d_r+k3[4],d_theta+k3[5], d_lambda+k3[6],m,vol, ro, r_b+k3[0], vvento_r, vvento_theta, vvento_lambda);
+
+        t += h;// O tempo é acrescido pelo passo h a cada loop
+        cont_p++; //Contador de impressão se soma
+
+        //Cálculo das variáveis
+        r_b += (k1[0]+2.0*k2[0]+2.0*k3[0]+k4[0])/6.0;
+        r += (k1[1]+2.0*k2[1]+2.0*k3[1]+k4[1])/6.0;
+        theta += (k1[2]+2.0*k2[2]+2.0*k3[2]+k4[2])/6.0;
+        lambda += (k1[3]+2.0*k2[3]+2.0*k3[3]+k4[3])/6.0;
+        d_r += (k1[4]+2.0*k2[4]+2.0*k3[4]+k4[4])/6.0;
+        d_theta += (k1[5]+2.0*k2[5]+2.0*k3[5]+k4[5])/6.0;
+        d_lambda += (k1[6]+2.0*k2[6]+2.0*k3[6]+k4[6])/6.0;
+
+        printf("%13.8lf %13.8lf %13.8lf %13.8lf %13.8lf\n",t , r, d_r, d_theta, d_lambda);
+
+        if (cont_p==10){ // A cada 100 loops do while os valores das variáveis são impressas no arquivo
+            if(arq==NULL){ //Mensagem caso haja erro na abertura do arquivo
+                printf("Problemas na abertura do arquivo\n");
+                system("pause");
+                exit(1);
+            }
+            //impressão de dados no arquivo
+            result = fprintf(arq,"%13.8lf %13.8lf %13.8lf %13.8lf %13.8lf %13.8lf\n",  t ,  r - R_T,  d_r,  ro, vol,  r_b);//troquei d_theta*r,  d_lambda*r*sin(theta) por ro e vol
+            if(result<0){
+                printf("Erro na escrita do arquivo\n");
+            }//
+            cont_p = 0; //Zera o contador
+        }
+        }
+        fclose(arq); //Fecha o arquivo
+        system("pause");
+    return 0;
+}
