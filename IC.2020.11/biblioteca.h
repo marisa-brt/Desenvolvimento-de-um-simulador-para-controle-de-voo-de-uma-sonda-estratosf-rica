534bb11adb73fe73c9331754769eebbaabf7b1ea
Marisa Fonseca de Almeida Brito <68127210+marisa-brt@users.noreply.github.com>
2020-11-22 20:34:05 -0300

    Add files via upload

---
+++ IC.2020.11/biblioteca.h
@@ -0,0 +1,30 @@
+//Biblioteca com funções necessárias para determinação da trajetória de voo de um balão meteorológico com hélio
+
+#define R_T 6371000 //Raio da Terra
+#define OMEGA 7.292115900231275e-5 // Velocidade angular da Terra
+#define R_h 2077.1 //Constante individual do hélio
+#define Ca 0.34037379532479679571 //Constante de arrasto
+#define Ra 287.54 // Constante universal dos gases tomando a massa molar do ar constante
+#define K 2.0 //Fator de viscosidade, determinado por parâmetros geométricos da válvula e número de Reynolds
+#define c_h 5193 //Calor específico do hélio [J/kg°C]
+#define c_b 1753 //Calor específic do látex [J/kgK]
+
+
+double grav(double r); //Função aceleração da gravidade local em m/s^2
+double update_atm(double alt_0, double T0, double r, double r_ant, double g, double *T, double *p, double *rho);//Determina as propriedades termodinâmicas da atmosfera na posicao radial r
+double Temp(double r); //Função temperatura por altitude em K
+double Pres(double r, double T, double g); //Função pressão por altitude em Pa
+double dens(double r, double T, double P, double g); //Função densidade por altitude em kg/m^3
+double dens_helio(double P, double T);//Densidade local do hélio
+double volume(double m_h, double ro_h); //Volume local do balão
+double raio_bi(double ro_h, double m_h); //Função raio do balão inicial
+double drb_dt(double r, double d_r, double T, double P, double r_b, double ro_h, double g, double m, double P_h, double drb_dpm, double alt_0, double T0);
+double arrasto_r(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda); //Função arrasto gerado pelo balão em r
+double arrasto_theta(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda); //Função arrasto gerado pelo balão em theta
+double arrasto_lambda(double v_r,double v_theta, double v_lambda, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda); //Função arrasto gerado pelo balão em lambda
+double d2r(double r, double theta, double d_r, double d_theta, double d_lambda, double m, double vol, double g, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda); //Aceleração radial
+double d2theta(double r, double theta, double d_r, double d_theta, double d_lambda, double m, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda); //Aceleração em theta
+double d2lambda(double r, double theta, double d_r,double d_theta, double d_lambda,double m, double vol, double ro, double r_b, double vvento_r, double vvento_theta, double vvento_lambda); //Aceleração em lambda
+double fluxo_massa(double ro_h, double d, double P, double P_h);
+double dQ_dt(double T, double T_h, double P, double P_h, double ro, double d_r, double r_b);
+
---
+++ IC.2020.11/simulador.c
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "biblioteca.h" //Biblioteca com funções necessárias
+
+/*
+    Para executar o código em sistemas windows você deve:
+    - Abrir a pasta com o arquivo executável (.exe). Na pasta original:IC.2020.10.1 -> bin -> Debug;
+    - Clicar em "Arquivo" no canto superior esquerdo e selecionar "Abrir o Windows PowerShell";
+    - Com o PowerShell aberto, digite .\(nome do arquivo). No arquivo original: .\IC.2020.10.exe
+    - Com a chamada do programa, digitar novamente .\(nome do arquivo) seguido das variáveis de lançamento pedidas separadas por espaço.
+
+*/
+
+//Código para cálculo da trajetória e velocidade em coordenadas esféricas do voo de um balão meteorológico com hélio
+int main(int argc, char* argv[])
+{
+    double r, theta, lambda, vol, m, alt, h, r_b, r_b0, e, e_0, m_h, g, T, T0, P, P_h, ro, ro_h, r_ant;
+    double d_r, d_lambda, d_theta, vvento_r, vvento_theta, vvento_lambda, drb_dpm, beta_e;
+    double Q, Cap, T_h;
+    double t = 0.0; //Tempo
+    int cont_p = 0.0;//Contador para impressão do resultado de 1000 a 1000 passos
+    h = 0.01;//Passo
+    int result;
+
+    /*if(argc != 9){
+        printf("Usar: IC.2020.11.exe altitude latitude(graus) longitude(graus) massa_balao[kg] carga_paga(payload[kg]) empuxo(kgf) Altitude de flutuação(m) Diâmetro da válvula (m)\n\n");
+        exit(1);
+    }*/
+
+    //Aplicação das variáveis dadas pelo usuário no código
+    double alt_0 = 850.0; //atof(argv[1]); //altitude de lançamento. Ex: 850.0;
+    double latitude = -22.00548;//atof(argv[2]);//latitude de lançamento. Ex: -22.00548;
+    double longitude = -47.93405;//atof(argv[3]);//longitude de lançamento. Ex: -47.93405;
+    double m_b = 1.6;//atof(argv[4]); //massa balão(sem hélio). Ex: 1.6;
+    double m_s = 3.5115;//atof(argv[5]); //payload. Ex: 3.5115;
+    double empuxo = 8.3905;//atof(argv[6]); //Ex: 8.390508;
+    double alt_flut = 25000;//atof(argv[6]); //Ex: 25000
+    double d = 0.02;//atof(argv[7]); //Diâmetro da válvula. Ex. 0.02
+
+    //Transformação dos dados para variávies do programa
+    r = alt_0 +R_T;
+    theta = M_PI/2 - latitude*M_PI/180;
+    lambda = longitude*M_PI/180;
+
+    //Determina-se derivadas iniciais
+    d_r = 0.0;
+    d_theta = 0.0;
+    d_lambda = 0.0;
+
+    //Determina-se as velocidades iniciais do vento (cte)
+    vvento_r = 0.0;
+    vvento_theta = 0.0;
+    vvento_lambda = 0.0;
+
+
+    FILE *arq; // chamando arquivo para escrita de dados
+    arq = fopen("Teste2.txt","w"); //Abrindo arquivo para escrita de dados
+
+    //Impressão do nome das variáveis no início do arquivo
+    result = fprintf(arq,"#       Tempo      Altitude    Velocidade    Massa         Temperatura        Massa hélio     T_h\n");
+
+    //Chamada de funções e determinação de variávies
+    g = grav(r); //aceleração da gravidade local
+    T0 = T = 22.0+273.15; //Alteracao 2020-11-05 Temp(r); //temperatura local
+    T_h = T + 0.05; //Temperatura do hélio
+    P =  94360; //Alteracao 2020-11-05 Pres(r, T, g); //pressão local
+    ro = P/(Ra*T); //densidade atmosférica local
+    ro_h = P/(R_h*T); //densidade local do hélio
+    vol = empuxo/ro; //volume inicial do balão. Ex: 7.5464;
+    m_h = ro_h * vol; //massa de hélio
+    m = m_b + m_s + m_h;
+    e_0 = e = 0.0005; //espessura da membrana de látex. Deve ser alterado com valores medidos!!
+    r_b = r_b0 = raio_bi(ro_h, m_h);
+    Q = 0.0;
+
+
+    // Determinação das "inclinações" dos intervalos para o método de Hunge Kutta através de vetores de 6 elementos
+    double k1[9],k2[9],k3[9],k4[9];
+
+    empuxo = ro*vol*(g+(r*OMEGA*OMEGA*sin(theta)*sin(theta)));
+    double peso = m*g;
+    //Impressão dos dados iniciais no arquivo
+    result = fprintf(arq,"%13.8lf %13.8lf %13.8lf %13.8lf %13.8lf %13.8lf %13.8lf\n",  t ,  r - R_T, d_r, m,  T, m_h, T_h);
+
+    //Cálculo dos vetores k's para cada elemento/variável posição, velocidade e raio do balão
+    while (r - R_T <= 32000){
+        //cálculo das variáves atmosféricas para cada altitude
+        alt = r - R_T;
+        g = grav(r);
+        update_atm(alt_0,T0,r,r_ant,g,&T,&P,&ro);
+        r_ant = r;
+        m_h = m - m_s - m_b;
+        ro_h = m_h/vol; //densidade do hélio
+        P_h = ro_h*R_h*T_h; //pressão dentro do balão adotando temperatura interna = externa
+
+        //Volume do balão - aproximação esférica
+        vol = 4.0*M_PI*pow(r_b,3.0)/3.0;
+
+        //Quantidade de calor transferida (balão->atmosfera)
+        Cap = c_h*m_h + c_b*m_b;//Capacidade térmica média do balão
+        T_h -= (Q/Cap);
+
+        //Modelo teórico de elasticidade da membrana de látex
+        e = e_0*r_b0*r_b0/(r_b*r_b); //cálculo da redução da espessura da membrana conforme crescimento do volume/raio do balão
+        beta_e = 50990.23198*pow(r_b, -4.885887); //coeficiente de elasticidade X espessura
+        drb_dpm = r_b*r_b/(2.0*beta_e - 3.0*r_b*(P_h-P)); //derivada do raio do balão pela pressão manométrica
+
+        //calculo de k1 para cada uma das variáveis
+        k1[0] = h*drb_dt(r, d_r, T, P, r_b, ro_h, g, m, P_h, drb_dpm, alt_0, T0);
+        k1[1] = h*d_r;
+        k1[2] = h*d_theta;
+        k1[3] = h*d_lambda;
+        k1[4] = h*d2r(r,theta,d_r,d_theta, d_lambda, m, vol, g, ro, r_b, vvento_r, vvento_theta, vvento_lambda);
+        k1[5] = h*d2theta(r,theta,d_r,d_theta,d_lambda,m,vol, ro, r_b, vvento_r, vvento_theta, vvento_lambda);
+        k1[6] = h*d2lambda(r,theta,d_r,d_theta,d_lambda,m,vol, ro, r_b, vvento_r, vvento_theta, vvento_lambda);
+        k1[7] = h*fluxo_massa(ro_h, d, P, P_h);
+        k1[8] = h*dQ_dt(T, T_h, P, P_h, ro, d_r, r_b);
+
+        //calculo k2
+        k2[0] = h*drb_dt(r+k1[1]/2.0, d_r+k1[4]/2.0, T, P, r_b+k1[0]/2.0, ro_h, g, m+k1[7]/2.0, P_h, drb_dpm, alt_0, T0);
+        k2[1] = h*(d_r+k1[4]/2.0);
+        k2[2] = h*(d_theta+k1[5]/2.0);
+        k2[3] = h*(d_lambda+k1[6]/2.0);
+        k2[4] = h*d2r(r+k1[1]/2.0,theta+k1[2]/2.0,d_r+k1[4]/2.0,d_theta+k1[5]/2.0, d_lambda+k1[6]/2.0,m+k1[7]/2.0,vol, g, ro, r_b+k1[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k2[5] = h*d2theta(r+k1[1]/2.0,theta+k1[2]/2.0,d_r+k1[4]/2.0,d_theta+k1[5]/2.0, d_lambda+k1[6]/2.0,m+k1[7]/2.0,vol, ro, r_b+k1[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k2[6] = h*d2lambda(r+k1[1]/2.0, theta+k1[2]/2.0, d_r+k1[4]/2.0,d_theta+k1[5]/2.0, d_lambda+k1[6]/2.0,m+k1[7]/2.0,vol, ro, r_b+k1[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k2[7] = h*fluxo_massa(ro_h, d, P, P_h);
+        k2[8] = h*dQ_dt(T, T_h, P, P_h, ro, d_r+k1[4]/2.0, r_b+k1[0]/2.0);
+
+        //calculo de k3
+        k3[0] = h*drb_dt(r+k2[1]/2.0, d_r+k2[4]/2.0, T, P, r_b+k2[0]/2.0, ro_h, g, m+k2[7]/2.0, P_h, drb_dpm, alt_0, T0);
+        k3[1] = h*(d_r+k2[4]/2.0);
+        k3[2] = h*(d_theta+k2[5]/2.0);
+        k3[3] = h*(d_lambda+k2[6]/2.0);
+        k3[4] = h*d2r(r+k2[1]/2.0,theta+k2[2]/2.0,d_r+k2[4]/2.0,d_theta+k2[5]/2.0, d_lambda+k2[6]/2.0,m+k2[7]/2.0,vol, g, ro, r_b+k2[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k3[5] = h*d2theta(r+k2[1]/2.0,theta+k2[2]/2.0,d_r+k2[4]/2.0,d_theta+k2[5]/2.0, d_lambda+k2[6]/2.0,m+k2[7]/2.0,vol, ro, r_b+k2[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k3[6] = h*d2lambda(r+k2[1]/2.0,theta+k2[2]/2.0,d_r+k2[4]/2.0,d_theta+k2[5]/2.0, d_lambda+k2[6]/2.0,m+k2[7]/2.0,vol, ro, r_b+k2[0]/2.0, vvento_r, vvento_theta, vvento_lambda);
+        k3[7] = h*fluxo_massa(ro_h, d, P, P_h);
+        k3[8] = h*dQ_dt(T, T_h, P, P_h, ro, d_r+k2[4]/2.0, r_b+k2[0]/2.0);
+
+        //calculo de k4
+        k4[0] = h*drb_dt(r+k3[1], d_r+k3[4], T, P, r_b+k3[0], ro_h, g, m+k3[7], P_h, drb_dpm, alt_0, T0);
+        k4[1] = h*(d_r+k3[4]);
+        k4[2] = h*(d_theta+k3[5]);
+        k4[3] = h*(d_lambda+k3[6]);
+        k4[4] = h*d2r(r+k3[1],theta+k3[2],d_r+k3[4],d_theta+k3[5], d_lambda+k3[6],m+k3[7],vol, g, ro, r_b+k3[0], vvento_r, vvento_theta, vvento_lambda);
+        k4[5] = h*d2theta(r+k3[1],theta+k3[2],d_r+k3[4],d_theta+k3[5], d_lambda+k3[6],m+k3[7],vol, ro, r_b+k3[0], vvento_r, vvento_theta, vvento_lambda);
+        k4[6] = h*d2lambda(r+k3[1],theta+k3[2], d_r+k3[4],d_theta+k3[5], d_lambda+k3[6],m+k3[7],vol, ro, r_b+k3[0], vvento_r, vvento_theta, vvento_lambda);
+        k4[7] = h*fluxo_massa(ro_h, d, P, P_h);
+        k4[8] = h*dQ_dt(T, T_h, P, P_h, ro, d_r+k3[4], r_b+k3[0]);
+
+        t += h;// O tempo é acrescido pelo passo h a cada loop
+        cont_p++; //Contador de impressão se soma
+
+        //Cálculo das variáveis
+        r_b += (k1[0]+2.0*k2[0]+2.0*k3[0]+k4[0])/6.0;
+        r += (k1[1]+2.0*k2[1]+2.0*k3[1]+k4[1])/6.0;
+        theta += (k1[2]+2.0*k2[2]+2.0*k3[2]+k4[2])/6.0;
+        lambda += (k1[3]+2.0*k2[3]+2.0*k3[3]+k4[3])/6.0;
+        d_r += (k1[4]+2.0*k2[4]+2.0*k3[4]+k4[4])/6.0;
+        d_theta += (k1[5]+2.0*k2[5]+2.0*k3[5]+k4[5])/6.0;
+        d_lambda += (k1[6]+2.0*k2[6]+2.0*k3[6]+k4[6])/6.0;
+        Q = (k1[8]+2.0*k2[8]+2.0*k3[8]+k4[8])/6.0;
+
+        //Vazão
+        if ((alt>alt_flut-1000)&&(d_r>0)){
+            m -= (k1[7]+2.0*k2[7]+2.0*k3[7]+k4[7])/6.0;
+        }
+
+        //empuxo = ro*vol*(g+(r*OMEGA*OMEGA*sin(theta)*sin(theta)));
+        //peso = m*g;
+
+        //Impressão das variáveis no arquivo
+        if (cont_p==1000){ // A cada 1000 loops do while os valores das variáveis são impressas no arquivo
+            if(arq==NULL){ //Mensagem caso haja erro na abertura do arquivo
+                printf("Problemas na abertura do arquivo\n");
+                system("pause");
+                exit(1);
+            }
+            printf("%13.8lf %13.8lf %13.8lf %13.8lf\n",t , r, d_r, m);//impressão de dados desejados na tela
+            //impressão de dados no arquivo
+            result = fprintf(arq,"%13.8lf %13.8lf %13.8lf %13.8lf %13.8lf %13.8lf %13.8lf\n",  t ,  r - R_T, d_r, m,  T, m_h, T_h);
+            if(result<0){
+                printf("Erro na escrita do arquivo\n");
+            }
+            cont_p = 0; //Zera o contador
+        }
+        }
+        fclose(arq); //Fecha o arquivo
+        system("pause");
+    return 0;
+}
+
